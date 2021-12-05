8c83cad49b52b769e70065fdbdd7ad36c78008b9
Cedric Morreau <cedricmorreau@gmail.com>
2021-12-06 00:19:11 +0100

    chore/ link contentful

--- styl/pages/index.js
+++ styl/pages/index.js
@@ -1,5 +1,52 @@
-export default function Home() {
+import { GraphQLClient, gql } from 'graphql-request'
+
+
+export const getStaticProps = async () => {
+  const endpoint = `https://graphql.contentful.com/content/v1/spaces/${process.env.CONTENTFUL_SPACE_ID}/`
+
+  const graphQLClient = new GraphQLClient(endpoint, {
+    headers: {
+      authorization: `Bearer ${process.env.CONTENFUL_ACCESS_KEY}`
+    },
+  })
+
+      const listingsQuery = gql`
+      {
+        pageCollection{
+          items {
+            welcome1
+            welcome2
+            welcome3
+            welcome4
+            title
+            tagline
+            logo{
+              url
+            }
+            sys{id}
+          }
+        }
+      }
+    `
+
+    const data = await graphQLClient.request(listingsQuery)
+  
+    return {
+      props: { homeContent: data.pageCollection.items }
+    }
+  }
+
+export default function Home({ homeContent }) {
+  console.log(homeContent)
+
   return (
-    <h1>hello world</h1>
+    <div>
+      {homeContent.map((homeContent) => <div
+        key={homeContent.sys.id}
+        >
+        
+        <h1>{homeContent.tagline}</h1>
+        </div>)}
+    </div>
   )
 }
