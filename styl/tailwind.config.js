63bf4418cff58092eba76129b57e510f788f2bf3
Cedric Morreau <cedricmorreau@gmail.com>
2021-12-06 19:17:25 +0100

    chore/ +font +homepage content

--- styl/tailwind.config.js
+++ styl/tailwind.config.js
@@ -3,10 +3,25 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        offWhite: "#ECF3E7",
+        black: "#212121",
+      },
+      fontFamily: {
+        ivy: "'ivypresto-display',serif;",
+        avenirMedium: "'avenirMedium',sans-serif;",
+        avenirMediumItalic: "'avenirMediumItalic',sans-serif;'",
+        avenirDemiBold: "'avenirDemiBold',sans-serif;'",
+      },
+      fill: theme => ({
+        "black": theme("colors.black")
+      }),
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
 }
+
